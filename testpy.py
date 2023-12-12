6a2f3f728e3c55aed309d53a2d3d6b6fd38037ae
shanvin-pomerleau <shanvin.luo@pomerleau.ca>
2023-12-12 17:41:19 -0500

    Create testpy.py

---
+++ testpy.py
@@ -0,0 +1,14 @@
+# Intentionally Vulnerable Python Code - SQL Injection
+
+def login(username, password):
+    # This is a simple example vulnerable to SQL injection
+    query = f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'"
+    # Imagine executing the query against a database, but here we'll just print it
+    print("Executing query:", query)
+
+# User input (simulating input from a form)
+user_input_username = "admin' OR '1'='1' --"
+user_input_password = "password123"
+
+# Calling the login function with user input
+login(user_input_username, user_input_password)
